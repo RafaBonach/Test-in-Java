e825baf0404f1884b25226eeb7bd9e7c2454c23c
RafaBonach <rafa.bonach@gmail.com>
2023-04-17 10:51:28 -0300

    Meu primeiro programa em Java!

--- Teste/src/primeiro/MeuPrimeiroPrograma.java
+++ Teste/src/primeiro/MeuPrimeiroPrograma.java
@@ -3,7 +3,13 @@ package primeiro;
 public class MeuPrimeiroPrograma {
 
 	public static void main(String[] args) {
-		System.out.print("Meu Primeiro Programa em Java");
+		int valor1, valor2, valor3;
+		valor1 = 10;
+		valor2 = 15;
+		valor3 = 5;
+		valor1 = valor1 + valor2;
+		valor3 = valor1 - valor3;
+		System.out.println("O resultado e " + valor3);
 	}
 
 }
